873b94594155dcb1d43b19aa3281d137e80707b3
7ue9ueue <23.tony.wu.swis@gmail.com>
2026-01-01 19:28:55 +0800

    NTT v2

--- core/arith_SIMD.cpp
+++ core/arith_SIMD.cpp
@@ -1,4 +1,5 @@
-v8i _mm256_multiply_mod(const v8i& a, const v8i& b) {
+// lazy Montgomery Multiplication， in [0, 2 * MOD)
+inline v8i _mm256_mont_multiply_mod(const v8i& a, const v8i& b) {
     v8i a_odd = _mm256_srli_epi64(a, 32);
     v8i b_odd = _mm256_srli_epi64(b, 32);
     v8i t_even = _mm256_mul_epu32(a, b);             
@@ -14,15 +15,23 @@ v8i _mm256_multiply_mod(const v8i& a, const v8i& b) {
     v8i adjusted = _mm256_sub_epi32(result, v_mod);
     return _mm256_min_epu32(result, adjusted);
 }
-
-v8i _mm256_add_mod(const v8i& a, const v8i& b) {
-    v8i adjusted = _mm256_sub_epi32(_mm256_add_epi32(a, b), v_mod);
+// a + b > m ? a + b - m : a + b
+v8i _mm256_add_mod(const v8i& a, const v8i& b, const v8i &m = v_wmod) {
+    v8i adjusted = _mm256_sub_epi32(_mm256_add_epi32(a, b), m);
     v8i mask = _mm256_srai_epi32(adjusted, 31);
-    return _mm256_add_epi32(adjusted, _mm256_and_si256(mask, v_mod));
+    return _mm256_add_epi32(adjusted, _mm256_and_si256(mask, m));
 }
 
-v8i _mm256_sub_mod(const v8i& a, const v8i& b) {
+// a - b < 0 ? m + a - b : a - b
+v8i _mm256_sub_mod(const v8i& a, const v8i& b, const v8i &m = v_wmod) {
     v8i diff = _mm256_sub_epi32(a, b);
     v8i mask = _mm256_cmpgt_epi32(b, a);
-    return _mm256_add_epi32(diff, _mm256_and_si256(mask, v_mod));
+    return _mm256_add_epi32(diff, _mm256_and_si256(mask, m));
+}
+
+// a > m ? a - m : a
+v8i _mm256_mod(const v8i& a, const v8i &m = v_mod) {
+    v8i diff = _mm256_sub_epi32(a, m);
+    v8i mask = _mm256_srai_epi32(diff, 31);
+    return _mm256_add_epi32(diff, _mm256_and_si256(mask, m));
 }
